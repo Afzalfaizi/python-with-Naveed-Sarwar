f64d68aa98804591001bfe1b8616fba5fbc5b02a
afzalfaizi <afzaalm993@gmail.com>
2024-02-14 19:24:00 +0500

    if else condition

--- class01/class1.py
+++ class01/class1.py
@@ -37,3 +37,64 @@ def addNumbers(a, b):
 
 print(addNumbers(2, 4))
 
+# if condition in python - with Naveed sarwar
+
+myName = "Muhammad Afzal"
+if  "Afzal" in myName:
+   print("Hello Muhammad Afzal")
+else:
+   print("I don't know you!")
+   
+   # create a login  system for user
+userName = input("Enter your username : ")
+password = input("Enter your password : ")
+
+if userName == "Muhammad Afzal" and password == "1122334455":
+    print("Login successful")
+elif userName == "guest" and password == "admin":
+    print("welcome to  guest account")
+else:
+    print("Invalid Username or Password")
+    
+ # if condition in python - with Naveed sarwar
+ 
+number = int(input("Enter Number"))
+num5 = number % 2 == 0
+if num5 :
+    print(number, "is even")
+else :
+    print(number, "is odd")
+    
+# write a programe that takes 3 numbers as an input from the user and find the largest number
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+num3 = int(input("Enter the third number : "))
+
+if num1 > num2 and num1 > num3:
+    print(num1, "is the largest number")
+elif num2 > num1 and num2 > num3:
+    print(num2, "is the largest number")
+else:
+    print(num3, "is the largest number")
+    
+# Match case condition in python
+n = int(input("Enter Day number"))
+match n:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid day")
+        
