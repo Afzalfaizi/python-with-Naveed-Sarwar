04b4ae7f813429c97ddf722d4dcebe551d252828
afzalfaizi <afzaalm993@gmail.com>
2024-02-16 17:57:28 +0500

    for loop

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+# For loop in python
+# What is for loop?
+# A for loop is a control flow structure that allows you to repeatedly execute a block of code
+# as long as an expression evaluates to True.
+# The syntax of the for loop consists of the keyword "for
+
+for index in range(0,11,1):
+    print("for loop",index)
+
+nums = [10,20,30,40,50,60,70,80]
+for index  in nums:
+    print("hello for loop", index)
