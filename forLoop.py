3dea295e769e347f87d1666099835b03e6b5209a
afzalfaizi <afzaalm993@gmail.com>
2024-02-16 18:58:40 +0500

    for loop (map, filter)

--- forLoop.py
+++ forLoop.py
@@ -1,20 +1,26 @@
-# For loop in python
-# What is for loop?
-# A for loop is a control flow structure that allows you to repeatedly execute a block of code
-# as long as an expression evaluates to True.
-# The syntax of the for loop consists of the keyword "for
+# # For loop in python
+# # What is for loop?
+# # A for loop is a control flow structure that allows you to repeatedly execute a block of code
+# # as long as an expression evaluates to True.
+# # The syntax of the for loop consists of the keyword "for
 
-for index in range(0,11,1):
-    print("for loop",index)
+# for index in range(0,11,1):
+#     print("for loop",index)
 
-nums = [10,20,30,40,50,60,70,80]
-for index  in nums:
-    print("hello for loop", index)
+# nums = [10,20,30,40,50,60,70,80]
+# for index  in nums:
+#     print("hello for loop", index)
     
-# table using for loop
+# # table using for loop
 
-num = int(input("Enter Table Number"))
-for i in range(1,11):
-    print(num,"*", i, "=", num*i)
+# num = int(input("Enter Table Number"))
+# for i in range(1,11):
+#     print(num,"*", i, "=", num*i)
 
-# table from user input
+# write a search programe, search the name of noman if found than loop should be stop.
+userNames = ["ali","hamza","mohsin","noman","nomi"]    
+for i,searchedName in enumerate(userNames) :
+    print("loop runing", searchedName,i)
+    if searchedName == "noman":
+        print("Name found", searchedName,i)
+        break
