ab3af0e2416c1c4a7956969be1d9706830a9b7a9
afzalfaizi <afzaalm993@gmail.com>
2024-02-17 12:09:38 +0500

    dictionaries in python

---
+++ dictionariers.py
@@ -0,0 +1,12 @@
+# Dictionaries in Python
+# What is Dictionaries in python?
+# A dictionary is a collection of key-value pairs.  
+# It can be thought of as an array, but instead of using indices to access elements,  we use keys to access values. 
+# It can be defined using curly brackets {} or by the type() function.  
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+}
+print(car["model"])
