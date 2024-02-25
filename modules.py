0feffefad82b85157dc5a7cf32a2ccf75ca39760
afzalfaizi <afzaalm993@gmail.com>
2024-02-25 18:49:26 +0500

    python modules

---
+++ modules.py
@@ -0,0 +1,9 @@
+import time
+currentTime = time.time( )  # get the current system time in seconds
+
+print("Current Time is :", currentTime)
+
+# import datetime 
+# currentTime = datetime.datetime.now()
+# print("Current Time is :", currentTime)
+# Python Modules (Time, Dates and operating system)
